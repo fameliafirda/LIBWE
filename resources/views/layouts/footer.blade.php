4b9b3c980d1165b7a598f19e03887794d5ee96f2
fameliafirda <fameliafirda69@gmail.com>
2026-04-30 05:52:32 +0700

    benerin rekom

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Main Footer -->
 <footer class="main-footer text-sm">
-  <strong>&copy; 2025 Perpustakaan SDN Berat Wetan 1.</strong>
+  <strong>&copy; Perpustakaan SDN Berat Wetan 1. | Famelia Firda Levia</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 1.0
